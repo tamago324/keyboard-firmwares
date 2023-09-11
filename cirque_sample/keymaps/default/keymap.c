7ae58ecdaea46bcdafef2dbd4869d91cdeda4d6c
tamago324 <tamago_pad@yahoo.co.jp>
2023-09-11 20:24:41 +0900

    update

--- cirque_sample/keymaps/default/keymap.c
+++ cirque_sample/keymaps/default/keymap.c
@@ -2,9 +2,26 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-          KC_Q, KC_W, KC_E, KC_R, KC_T,    KC_Y, KC_U,  KC_I,    KC_O,   KC_P,
-          KC_A, KC_S, KC_D, KC_F, KC_G,    KC_H, KC_J,  KC_K,    KC_L,   KC_SCLN,
-          KC_Z, KC_X, KC_C, KC_V, KC_B,    KC_N, KC_M,  KC_COMM, KC_DOT, KC_SLSH,
-                      KC_TAB, KC_SPACE,    KC_ENT, KC_BSPC
+          KC_A
     )
 };
+
+void keyboard_pre_init_user(void) {
+// void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  debug_keyboard=true;
+  debug_mouse=true;
+}
+
+// report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+//     dprint("pointing_device_task_user called\n");
+//     dprintf("X: %d, Y: %d, Buttons: %d\n", mouse_report.x, mouse_report.y, mouse_report.buttons);
+//     return mouse_report;
+// }
+
+
+void pointing_device_init_user(void) {
+  dprint("pointing_device_init_user called\n");
+}
