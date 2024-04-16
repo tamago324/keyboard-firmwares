ad6c90791d656a43af1415f79cdb13e77792a25c
tamago324 <tamago_pad@yahoo.co.jp>
2024-04-16 13:13:21 +0000

    __qmk__ と __vial__ を submodule として追加

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,20 +2,6 @@
 
 cd /workspace
 
-# qmk
-if [ ! -d "__qmk__" ]; then
-    git clone --recursive https://github.com/qmk/qmk_firmware __qmk__
-    qmk setup --home __qmk__ --yes
-    qmk git-submodule
-fi
-
-# vial
-if [ ! -d "__vial__" ]; then
-    git clone --recursive https://github.com/vial-kb/vial-qmk __vial__
-    cd __vial__
-    make git-submodule
-fi
-
 # link keybaords files
 QMK_KEYBOARDS_DIR="/workspace/__qmk__/keyboards"
 VIAL_KEYBOARDS_DIR="/workspace/__vial__/keyboards"
